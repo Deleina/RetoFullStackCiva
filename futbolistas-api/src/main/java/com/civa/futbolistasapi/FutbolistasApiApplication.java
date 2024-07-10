b33de79380ce7e281f75789b2880628de0005ea3
Deleina <marta.deleina@gmail.com>
2024-07-10 17:49:20 -0500

    - Estructura básica del proyecto configurada.

---
+++ futbolistas-api/src/main/java/com/civa/futbolistasapi/FutbolistasApiApplication.java
@@ -0,0 +1,13 @@
+package com.civa.futbolistasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutbolistasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutbolistasApiApplication.class, args);
+	}
+
+}
